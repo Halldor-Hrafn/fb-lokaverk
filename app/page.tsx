827c4b152882ef4b23986ca5eded7ad48bef9fd8
Halldór <halldor.hrafn.reynisson@gmail.com>
2024-01-22 19:40:13 +0000

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,16 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
+import FixedNavbar from "@/components/FixedNavbar";
+
 import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/SignUpUserSteps";
-import Header from "@/components/Header";
 import { cookies } from "next/headers";
 
 export default async function Index() {
-  const cookieStore = cookies();
-
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
-
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
-        </div>
-      </nav>
+      <FixedNavbar />
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+          <h2 className="font-bold text-4xl mb-4">Welcome, enjoy your stay</h2>
         </main>
       </div>
 
---
+++ components/FixedNavbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AuthButton from "./AuthButton";
+
+const FixedNavbar: React.FC = () => {
+  return (
+    <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+      <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+        <AuthButton />
+      </div>
+    </nav>
+  )
+}
+
+export default FixedNavbar;
