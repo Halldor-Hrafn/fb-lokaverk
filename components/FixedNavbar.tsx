b6f008d7211b9754775d4f6c12da7c102a932380
Halldór <halldor.hrafn.reynisson@gmail.com>
2024-01-29 19:50:50 +0000

    Added a Home link in the navbar

--- components/FixedNavbar.tsx
+++ components/FixedNavbar.tsx
@@ -5,7 +5,10 @@ const FixedNavbar: React.FC = () => {
   return (
     <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
       <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-        <a href="/courses">
+        <a href="/" className="transition-transform duration-200 hover:scale-110">
+          Home
+        </a>
+        <a href="/courses" className="transition-transform duration-200 hover:scale-110">
           Courses
         </a>
         <AuthButton />
