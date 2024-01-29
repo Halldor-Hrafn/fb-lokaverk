22e9644c5a8ff7a0b074bc3ac84fda764f59ca53
Halldór <halldor.hrafn.reynisson@gmail.com>
2024-01-29 17:59:59 +0000

    Added the courses link to the navbar

--- components/FixedNavbar.tsx
+++ components/FixedNavbar.tsx
@@ -5,6 +5,9 @@ const FixedNavbar: React.FC = () => {
   return (
     <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
       <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+        <a href="/courses">
+          Courses
+        </a>
         <AuthButton />
       </div>
     </nav>
