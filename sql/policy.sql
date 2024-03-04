8d3405d7303d25ac681ae350b1f9d70020cb7726
Halldór <halldor.hrafn.reynisson@gmail.com>
2024-03-04 18:39:54 +0000

    Added new policies to policy.sql

--- sql/policy.sql
+++ sql/policy.sql
@@ -2,6 +2,7 @@ ALTER TABLE public.courses ENABLE ROW LEVEL SECURITY;
 ALTER TABLE public.notes ENABLE ROW LEVEL SECURITY;
 ALTER TABLE public.admins ENABLE ROW LEVEL SECURITY;
 ALTER TABLE public.profiles ENABLE ROW LEVEL SECURITY;
+ALTER TABLE public.questions ENABLE ROW LEVEL SECURITY;
 
 CREATE POLICY "Everyone can read courses" ON public.courses
     FOR SELECT
@@ -28,4 +29,13 @@ CREATE POLICY "Everyone can view profiles" ON public.profiles
 CREATE POLICY "Everyone can create profiles" ON public.profiles
     FOR INSERT
     TO anon
+    WITH CHECK (true);
+
+CREATE POLICY "Everyone can read questions" ON public.questions
+    FOR SELECT
+    USING (true);
+
+CREATE POLICY "Authenticated can create questions" ON public.questions
+    FOR INSERT
+    TO authenticated
     WITH CHECK (true);
