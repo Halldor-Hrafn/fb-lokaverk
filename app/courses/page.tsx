a20406638a3dcbc9d0625a8cc63c9351201cc765
Halldór Hrafn Reynisson <halldor.hrafn.reynisson@gmail.com>
2024-03-14 18:26:42 +0000

    bla

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -16,7 +16,7 @@ export default async function Page() {
     .select();
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+    <div className="flex-1 w-full flex flex-col gap-20">
       <FixedNavbar />
       {data && (
         <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
--- components/Course.tsx
+++ components/Course.tsx
@@ -10,11 +10,12 @@ interface Props {
 const Course: React.FC<Props> = ({ index, title, initials, link}) => {
   return (
     <div key={index} className="flex flex-col gap-2">
-      <h3 className="font-bold text-2x1">{title}</h3>
-      <p className="text-lg">{initials}</p>
-      <a href={`/courses/${initials}`} className="text-lg underline">{initials}</a>
+      <div className="flex flex-row gap-4 items-center border rounded px-3">
+        <h3 className="font-bold text-2x1">{title}</h3>
+        <a href={`/courses/${initials}`} className="text-lg underline">{initials}</a>
+      </div>
     </div>
   )
 }
-
+nlksajjflkajfjfkjshiwquioqhlkjcojfoijfoiejfoi
 export default Course;
